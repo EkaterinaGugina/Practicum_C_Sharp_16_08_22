775ec38325d4861865d27e60e8effa2583835cbc
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-17 02:33:27 +0500

    add void from Ex27

---
+++ HomeWork_16_08_22/Ex27_Sum_numb/Program.cs
@@ -0,0 +1,21 @@
+﻿// Ex27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.Write("Введите число, number = ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNum(int num)
+{
+    int sum = 0;
+    while (num >= 0)
+    {
+        int n = num % 10;
+        sum += n;
+        num /= 10;
+    }
+   return sum; 
+}
+int s = SumNum(number);
+Console.Write("Сумма цифр этого числа равна  ");
+Console.WriteLine(s);
