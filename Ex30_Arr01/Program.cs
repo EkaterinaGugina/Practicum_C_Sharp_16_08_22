345eca1a337aa64e945176adbc8ac3d5ef5fd2e9
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-17 20:13:17 +0500

    decide Уч30

--- Ex30_Arr01/Program.cs
+++ Ex30_Arr01/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ex30 - Программа получает на вход массив из 8 элементов, заполняет его 0 и 1 случайным образом и выводит его на экран.
+int[] newarray = new int[8];
+void ArrayIn01(int arr01)
+{
+    for (int i = 0; i < arr01.Length; i++)
+    {
+        arr01[i] =  new Random().Next(0, 2);
+        Console.Write($"{arr01[i]} ");
+    } 
+}   
+
+Console.Write("[");
+ArrayIn01(newarray);
+Console.Write("]");
