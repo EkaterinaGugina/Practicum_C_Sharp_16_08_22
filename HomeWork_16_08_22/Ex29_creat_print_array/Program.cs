d4daa76eb78a382a1e7a171c5875082cae4f281b
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-17 19:46:37 +0500

    deleted error from Readme

--- HomeWork_16_08_22/Ex29_creat_print_array/Program.cs
+++ HomeWork_16_08_22/Ex29_creat_print_array/Program.cs
@@ -6,25 +6,17 @@
 
 //Ввод массива с консоли
 Console.WriteLine("Заполним массив. Введите 8 элементов массива через Enter");
-int length = 8;
-int[] newarray = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-for (int index = 0; index < length; index++)
+int[] newarray = new int[8];
+for (int index = 0; index < newarray.Length; index++)
     {
         int num = Convert.ToInt32(Console.ReadLine());
         newarray[index] = num;
     } 
     
-//Вывод массива на экран
+//Вывод массива на экран в квадратных скобках
 int count = newarray.Length;
 Console.Write("[");
-for (int position = 0; position < 4; position++)
-    {
-        Console.Write($"{newarray[position]}, ");
-    } 
-Console.Write($"{newarray[4]}]"); 
-Console.WriteLine();  
-Console.Write("[");    
-for (int position = 5; position < 7; position++)
+for (int position = 0; position < 7; position++)
     {
         Console.Write($"{newarray[position]}, ");
     } 
