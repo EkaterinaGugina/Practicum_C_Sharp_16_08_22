ca0af8a93cc781ee4421d8ab9d26561572bba1e1
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-17 20:41:18 +0500

    Ex25 update

--- HomeWork_16_08_22/Ex25_number_degre/Program.cs
+++ HomeWork_16_08_22/Ex25_number_degre/Program.cs
@@ -2,18 +2,18 @@
 //3, 5 -> 243 (3⁵)
 
 Console.WriteLine("Введите два числа");          //ввод данных
-Console.Write("num1 =");
+Console.Write("A =");
 int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("num1 =");
+Console.Write("B =");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 void Num1DegreeNum2(int n1, int n2)             //метод возведения n1 в степень n2
 {
     int result = 1;
-    for (int count = 1; count <= n2; count++)
+    for (int i = 1; i <= n2; i++)
     {
         result = result * n1;
     }
-    Console.WriteLine($"{n1}^{n2} = {result}");
+    Console.WriteLine($"{n1} ^ {n2} = {result}");
 }
-Num1DegreeNum2(num1, num2);
+Num1DegreeNum2(A, B);
