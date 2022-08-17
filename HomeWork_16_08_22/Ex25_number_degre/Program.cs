d9c394a86acda99f66264289a9549f4d0c6a1052
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-17 20:29:56 +0500

    initial commit Ex25

--- HomeWork_16_08_22/Ex25_number_degre/Program.cs
+++ HomeWork_16_08_22/Ex25_number_degre/Program.cs
@@ -1,14 +1,19 @@
 ﻿// Ex25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 //3, 5 -> 243 (3⁵)
-//2, 4 -> 16
-Console.WriteLine("Введите два числа");
+
+Console.WriteLine("Введите два числа");          //ввод данных
 Console.Write("num1 =");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("num1 =");
 int num2 = Convert.ToInt32(Console.ReadLine());
-int result = 1;
-for (int count = 1; count <= num2; count++)
+
+void Num1DegreeNum2(int n1, int n2)             //метод возведения n1 в степень n2
+{
+    int result = 1;
+    for (int count = 1; count <= n2; count++)
     {
-        result = result * num1;
-    } 
-Console.WriteLine($"{num1}^{num2} = {result}");
+        result = result * n1;
+    }
+    Console.WriteLine($"{n1}^{n2} = {result}");
+}
+Num1DegreeNum2(num1, num2);
