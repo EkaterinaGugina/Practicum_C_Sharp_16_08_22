c65eda840f434f1b251cc35fd2bd84d0665681dd
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-17 19:53:33 +0500

    Ex26 solved

--- Ex26_Kolvo_numbers/Program.cs
+++ Ex26_Kolvo_numbers/Program.cs
@@ -1,15 +1,15 @@
-﻿// Ex26 - Прорамма получает на вход число и выводит количество цифр этого числа.
+﻿// Ex26 - Программа получает на вход число и выводит количество цифр этого числа.
 Console.Write("Введите число, number = ");
 int number = Convert.ToInt32(Console.ReadLine());
-
 int CountNum(int num)
 {
     int count = 0; 
-    while (num >= 0)
+    while (num > 0)
     {
         num = num / 10;
-        count += 1; 
+        count++; 
     }
    return count; 
 }
-Console.Write($"Количество цифр числа {number} равно {CountNum(number)}");
+int n = CountNum(number);
+Console.Write($"Количество цифр числа {number} равно {n}");
