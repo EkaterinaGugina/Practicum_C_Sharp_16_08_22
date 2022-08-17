2fc87b99a67eb85f5a173dd9e175fd1d6851de65
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-17 20:21:12 +0500

    Ex30 solved

--- Ex30_Arr01/Program.cs
+++ Ex30_Arr01/Program.cs
@@ -1,14 +1,13 @@
 ﻿// Ex30 - Программа получает на вход массив из 8 элементов, заполняет его 0 и 1 случайным образом и выводит его на экран.
-int[] newarray = new int[8];
-void ArrayIn01(int arr01)
+void ArrayIn01(int[] arr01)
 {
     for (int i = 0; i < arr01.Length; i++)
     {
-        arr01[i] =  new Random().Next(0, 2);
+        arr01[i] = new Random().Next(0, 2);
         Console.Write($"{arr01[i]} ");
     } 
 }   
-
+int[] newarray = new int[8];
 Console.Write("[");
 ArrayIn01(newarray);
 Console.Write("]");
