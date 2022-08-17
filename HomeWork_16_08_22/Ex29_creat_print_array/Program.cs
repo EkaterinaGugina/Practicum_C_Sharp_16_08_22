2b0cb720574e2804daa6d8ea7536481452a05b3d
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-17 21:40:00 +0500

    update folder Practicum

--- HomeWork_16_08_22/Ex29_creat_print_array/Program.cs
+++ HomeWork_16_08_22/Ex29_creat_print_array/Program.cs
@@ -1,23 +1,42 @@
 ﻿// Ex29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
-   // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-
-   // 6, 1, 33 -> [6, 1, 33]
-
-//Ввод массива с консоли
-Console.WriteLine("Заполним массив. Введите 8 элементов массива через Enter");
-int[] newarray = new int[8];
-for (int index = 0; index < newarray.Length; index++)
+void PrintNewArray(int[] arr);
+{
+    for (int i = 0; i < arr.Length; i++)
     {
         int num = Convert.ToInt32(Console.ReadLine());
-        newarray[index] = num;
-    } 
-    
-//Вывод массива на экран в квадратных скобках
-int count = newarray.Length;
-Console.Write("[");
-for (int position = 0; position < 7; position++)
+        arr[i] = num;
+    }
+    Console.Write("[");                             //Вывод массива на экран в квадратных скобках
+    for (int i = 0; i < arr.Length - 1; i++)
     {
-        Console.Write($"{newarray[position]}, ");
-    } 
-Console.WriteLine($"{newarray[7]}]"); 
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine($"{arr[i]}]");
+}
+
+int[] newarray = new int[8];
+Console.WriteLine("Заполним массив. Введите 8 элементов массива через Enter"); //ввод массива с консоли
+PrintNewArray(newarray);
+
+
+
+
+
+
+//for (int i = 0; i < length; i++)
+   // {
+   //     int num = Convert.ToInt32(Console.ReadLine());
+//       array[i] = num;
+ //   }
+//    int count = length;       //Вывод массива на экран в квадратных скобках
+//    Console.Write("[");
+//    for (int position = 0; position < 7; position++)
+//    {
+//        Console.Write($"{newarray[position]}, ");
+//    }
+//    Console.WriteLine($"{newarray[7]}]");
+//}
+
+//int[] newarray = new int[8];
