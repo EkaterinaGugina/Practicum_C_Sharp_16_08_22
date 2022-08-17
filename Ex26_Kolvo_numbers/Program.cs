b1f8039d38a2a19d2b514ad79655ef3bcd914f8f
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-17 18:38:01 +0500

    deside Ex26

--- Ex26_Kolvo_numbers/Program.cs
+++ Ex26_Kolvo_numbers/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ex26 - Прорамма получает на вход число и выводит количество цифр этого числа.
+Console.Write("Введите число, number = ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int CountNum(int num)
+{
+    int count = 0; //если не выполнится цикл, будет ясно, что в него не заходили
+    for (int i = 1; num >= 10; i++)
+    {
+        num = num / 10;
+        count = i +1; 
+    }
+   return count; 
+}
+Console.Write($"Количество цифр числа {number} равна {CountNum(number)}");
